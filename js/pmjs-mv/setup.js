51fa73b94ba851d91e7c110592e4e03c260b3e87
bread <136384195+bbbreaddd@users.noreply.github.com>
2026-09-13 02:46:35 -0700

    add Intl warmup script

---
+++ js/pmjs-mv/setup.js
@@ -0,0 +1,8 @@
+function warmIntl(config) {
+  if (!config) return;
+  if (config.numberEn) (1234567).toLocaleString('en');
+  if (config.dateEn) new Date(0).toLocaleDateString('en');
+  if (config.collatorEn) 'b'.localeCompare('a', 'en');
+}
+
+warmIntl((globalThis.PMJS_GAME_CONFIG || {}).intlWarmup);
